c689f9b2830f43f7a51b9a3ba73f0e5914fe9c2d
Bernard Chu <bernard.chu@yahoo.com>
2014-12-01 11:28:05 -0800

    Added placeholder functions for GET and POST requests.

--- server/server.js
+++ server/server.js
@@ -19,12 +19,26 @@ app.set('port', 3000);
 app.use(morgan('dev'));
 app.use(parser.json());
 
+// GET and POST functions
+var toggleIngredient = function() {
+  console.log('toggleIngredient called');
+};
+
+var getIngredients = function() {
+  console.log('getIngredients called');
+};
+
+var getDrinks = function() {
+  console.log('getDrinks called');
+};
 // Set up our routes
 // app.use("/classes", router);
+app.post('/ingredients', toggleIngredient);
+app.get('/ingredients', getIngredients);
+app.get('/drinks', getDrinks);
 
 // Serve the client files
 app.use(express.static(__dirname + "../../client"));
-
 
 // If we are being run directly, run the server.
 if (!module.parent) {
